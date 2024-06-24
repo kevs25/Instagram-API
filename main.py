2f4e0cd6b853be28b94006d774ced69dbe03ed5b
kevs25 <kevindaniel200225@gmail.com>
2024-06-24 21:22:46 +0530

    changes in cors origin

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ['*'],
     allow_credentials = True,
     allow_methods = ['*'],
     allow_headers = ['*']
